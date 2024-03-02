a7036b7cc4e8cf74de38e1a47748cc9f6cbe1d56
Funnisimo <scastette@yahoo.com>
2024-03-01 22:40:24 -0600

    simplified DelayedSystem

--- src/system/DelayedSystem.ts
+++ src/system/DelayedSystem.ts
@@ -1,34 +1,12 @@
-import { System } from "./system";
+import { IntervalSystem } from "./intervalSystem";
 
-export abstract class DelayedSystem extends System {
-  protected delay: number;
-
+export abstract class DelayedSystem extends IntervalSystem {
   public constructor(delay: number) {
-    super();
-    this.delay = delay;
-  }
-
-  protected updateDelay() {
-    this.delay -= this.world.delta;
-  }
-
-  public process(): void {
-    if (this.isEnabled()) {
-      this.updateDelay();
-      if (this.delay <= 0) {
-        this.beforeProcess();
-        this.doProcess();
-        this.afterProcess();
-        this.delay = 0;
-        this.setEnabled(false);
-      }
-    }
+    super(0, delay);
   }
 
   public runIn(delay: number) {
     this.delay = delay;
     this.setEnabled(true);
   }
-
-  protected abstract doProcess(): void;
 }
--- src/system/IntervalSystem.ts
+++ src/system/IntervalSystem.ts
@@ -5,10 +5,10 @@ export abstract class IntervalSystem extends System {
   protected interval: number;
   private catchUp: boolean = true;
 
-  public constructor(interval: number, delay?: number) {
+  public constructor(runEvery: number, startIn?: number) {
     super();
-    this.interval = interval;
-    this.delay = delay === undefined ? interval : delay;
+    this.interval = runEvery;
+    this.delay = startIn === undefined ? runEvery : startIn;
   }
 
   protected updateDelay() {
@@ -26,10 +26,14 @@ export abstract class IntervalSystem extends System {
         this.beforeProcess();
         this.doProcess();
         this.afterProcess();
-        this.delay += this.interval;
-        // TODO - there is a way to do this without the loop
-        while (this.delay < 0 && !this.catchUp) {
+        if (this.interval == 0) {
+          this.setEnabled(false);
+        } else {
           this.delay += this.interval;
+          // TODO - there is a way to do this without the loop
+          while (this.delay < 0 && !this.catchUp) {
+            this.delay += this.interval;
+          }
         }
       }
     }
